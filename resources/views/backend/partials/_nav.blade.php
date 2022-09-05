3c6c8d9322db91057e0296e8e2a51678c3d1614c
Nazmul haque <bpinazmul18@gmail.com>
2022-09-05 12:14:33 +0600

    Fixed navbar user name and role.

--- resources/views/backend/partials/_nav.blade.php
+++ resources/views/backend/partials/_nav.blade.php
@@ -14,9 +14,12 @@
 
     <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown user-menu">
-            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                <span class="d-none d-md-inline">{{ \Illuminate\Support\Facades\Auth::user()->name }}</span>
-                <img src="{{ asset('AdminLTE-3.2.0/dist/img/user2-160x160.jpg') }}" class="user-image img-circle elevation-2" alt="User Image">
+            <a href="#" class="nav-link dropdown-toggle d-flex align-items-center justify-content-center" data-toggle="dropdown">
+                <div class="d-flex flex-column align-items-center pr-2">
+                    <span class="custom_font_size_sm d-none d-md-inline">{{ \Illuminate\Support\Facades\Auth::user()->name }}</span>
+                    <small class="custom_font_size_xs align-self-end">System admin</small>
+                </div>
+                <img src="{{ asset('AdminLTE-3.2.0/dist/img/user2-160x160.jpg') }}" class="user-image img-circle elevation-2 m-0" alt="User Image">
 
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -29,7 +32,7 @@
                     </p>
                 </li>
 
-                <li class="user-footer">
+                <li class="user-footer d-flex justify-content-center">
                     <div class="float-start">
                         <a href="{!! route('user.show', \Illuminate\Support\Facades\Auth::user()->id) !!}" class="btn btn-default btn-flat">Profile</a>
                     </div>
