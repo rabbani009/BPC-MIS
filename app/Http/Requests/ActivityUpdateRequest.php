ecb063b684b22760dcf838fd3171ddc57fd6f664
rabbani <farzad.edsoft@gmail.com>
2022-10-16 13:08:54 +0600

    Activity update done Except number of trainee

--- app/Http/Requests/ActivityUpdateRequest.php
+++ app/Http/Requests/ActivityUpdateRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ActivityUpdateRequest extends FormRequest
@@ -13,7 +14,8 @@ class ActivityUpdateRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        // return false;
+        return Auth::check();
     }
 
     /**
@@ -24,7 +26,24 @@ class ActivityUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'council' => 'required|exists:councils,id',
+            'association' => 'required|exists:associations,id',
+            'program' => 'required|exists:programs,id',
+
+            'activity_title' => 'required',
+            'remarks' => 'required|boolean',
+            'start_date' => 'nullable|date_format:Y-m-d|before:tomorrow',
+            'end_date' => 'nullable|date_format:Y-m-d|after:start_date',
+            'venue' => 'nullable',
+            'number_of_trainers' => 'nullable|integer',
+            'trainers' => 'nullable|array',
+            'number_of_trainees' => 'nullable|integer',
+            'trainees' => 'nullable|array',
+
+            'source_of_fund' => 'nullable',//Dropdown 1. GOB, 2. Development budgets 3.Council Association 4. Others
+            'budget_as_per_contract' => 'nullable|digits_between:1,99999999999999',
+            'actual_budget_as_per_expenditure' => 'nullable|digits_between:1,99999999999999',
+            'actual_expenditure_as_per_actual_budget' => 'nullable|digits_between:1,99999999999999',
         ];
     }
 }
