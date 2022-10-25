64860f3e1e32b65d38cbe1fd2683fdc837f0061c
rabbani <farzad.edsoft@gmail.com>
2022-10-25 14:54:48 +0600

    footer updates

--- resources/views/backend/partials/_footer.blade.php
+++ resources/views/backend/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0
+        <b>Business</b>Promotion Council.
     </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Developed By &copy; 2021-2022 <a href="https://www.radissonbd.com/">radissonBD</a>.</strong> All rights reserved.
 </footer>
