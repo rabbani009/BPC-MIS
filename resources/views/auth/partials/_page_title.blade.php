a99c6cd56e0c93ac671544673c394ab4d86aa4a4
rabbani <farzad.edsoft@gmail.com>
2022-10-30 11:13:43 +0600

    login-page height & width

--- resources/views/auth/partials/_page_title.blade.php
+++ resources/views/auth/partials/_page_title.blade.php
@@ -1,1 +1,4 @@
-<a href="#"><b>BPC</b>MIS</a>
+{{-- <a href="#"><b>BPC</b>MIS</a> --}}
+
+<a href="#" src=""> <img src="{{ asset('Custom/img/logo.png') }}" style="height: 100px;
+    width: 50%;" alt="Business promotion council" class="navbar-brand"></a>
